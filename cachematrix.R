a5bfc2c168ba6f0ec1691c3ec6aff7bc6c44b892
Umesh Kasture <umesh.kastuer@gmail.com>
2017-07-18 08:17:42 +0530

    Commented

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## MakeCached Matrix creates data structure "m" to cache an inverse of square matrix
+## Default value of "m" is set to NULL for testing "get" and "set" functions are implemented for
+## matrix data and it's inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
-  }
-  get <- function() x
-  setInvr <- function(Invr) m <<- Invr
-  getInvr <- function() m
-  list(set = set, get = get,
-       setInvr = setInvr,
-       getInvr = getInvr)
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function()
+        x
+    setInvr <- function(Invr)
+        m <<- Invr
+    getInvr <- function()
+        m
+    list(
+        set = set,
+        get = get,
+        setInvr = setInvr,
+        getInvr = getInvr
+    )
 }
 
 
-## Write a short comment describing this function
+## cacheSolve replaces solve(x) to provide inverse of square matrix
+## Prior call to makeCacheMatix expected. Inverse computed if stored value found to be NULL (as initialized)
+## Value of inverse stored on first computation
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-  m <- x$getInvr()
-  if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
-  }
-  data <- x$get()
-  m <- solve(data, ...)
-  x$setInvr(m)
-  m
-  }
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getInvr()
+    if (!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setInvr(m)
+    m
+}
